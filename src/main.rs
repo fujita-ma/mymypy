79db140a908e370ba1e3751f35df2a55c6f0f2d3
fujita-ma <>
2019-12-01 18:08:51 +0900

    add comment

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ extern crate difference;
 use regex::{Regex, Captures};
 use difference::{Difference, Changeset};
 
+/// execute mypy and return stdout as String
 fn exec_mypy(file_name: &str, shadow_name: &str) -> io::Result<String>{
     let mypy_output = Command::new("mypy")
         .args(&["--strict", "--shadow-file", &shadow_name, &file_name, &shadow_name])
@@ -21,15 +22,16 @@ fn exec_mypy(file_name: &str, shadow_name: &str) -> io::Result<String>{
     Ok(text)
 }
 
-fn create_tmp_file_from_git<'a>(handle: &'a mut NamedTempFile, ref_name: &str, file_name: &str) -> &'a str{
+/// write bytes from git show ref_name:file_name
+fn write_file_from_git(handle: &mut NamedTempFile, ref_name: &str, file_name: &str) -> (){
     let output = Command::new("git")
         .args(&["--no-pager", "show", &format!("{}:{}", ref_name, &file_name)])
         .output()
         .expect("failed to execute process");
     handle.write_all(&output.stdout[..]).expect("failed to write");
-    handle.path().to_str().unwrap()
 }
 
+/// get int -> int map, which projects line index of original file to edited file
 fn calc_index_map(diff_output: Vec<u8>) -> HashMap<usize, usize>{
     let diff_string = String::from_utf8(diff_output).unwrap();
     let diff_splitted = diff_string.split("\n")
@@ -40,16 +42,17 @@ fn calc_index_map(diff_output: Vec<u8>) -> HashMap<usize, usize>{
         .filter(|&line| !line.starts_with("+"))
         .enumerate()
         .filter(|(_, line)| !line.starts_with("-"))
-        .map(|(idx, _)| idx);
+        .map(|(idx, _)| idx + 1);
     let unchanged_line_idx_edit = diff_splitted.clone()
         .filter(|&line| !line.starts_with("-"))
         .enumerate()
         .filter(|(_, line)| !line.starts_with("+"))
-        .map(|(idx, _)| idx);
+        .map(|(idx, _)| idx + 1);
 
     unchanged_line_idx_orig.zip(unchanged_line_idx_edit).collect()
 }
 
+/// display diff with color in terminal
 fn print_diff(orig: &str, edit: &str) -> io::Result<()>{
     let Changeset { diffs, .. } = Changeset::new(&orig, &edit, "\n");
 
@@ -85,6 +88,7 @@ fn main() {
     };
     let edit_ref = args.get(2);
 
+    // extract edited filenames
     let filename_output = Command::new("git")
         .args(match edit_ref{
             Some(ref_name) => vec!["diff", "--name-only", orig_ref, ref_name],
@@ -97,20 +101,25 @@ fn main() {
     for name in filenames.split("\n").filter(|&line| line.ends_with(".py")){
         println!("{}", &name);
         
+        // get mypy result of original file
         let mut orig_tmp_file = NamedTempFile::new().unwrap();
-        let orig_name = create_tmp_file_from_git(&mut orig_tmp_file, orig_ref, name);
+        write_file_from_git(&mut orig_tmp_file, orig_ref, name);
+        let orig_name = orig_tmp_file.path().to_str().unwrap();
         let orig_mypy_string = exec_mypy(orig_name, name).expect("failed to exec mypy");
 
+        // get mypy result of edited file
         let mut edit_tmp_file: NamedTempFile;
         let edit_name = match edit_ref{
             Some(ref_name) => {
                 edit_tmp_file = NamedTempFile::new().unwrap();
-                create_tmp_file_from_git(&mut edit_tmp_file, ref_name, name)
+                write_file_from_git(&mut edit_tmp_file, ref_name, name);
+                edit_tmp_file.path().to_str().unwrap()
             },
             None => name
         };
         let edit_mypy_string = exec_mypy(edit_name, name).expect("failed to exec mypy");
 
+        // map line indices that are invariant between the two files
         let diff_output = Command::new("git")
         .args(match edit_ref{
             Some(ref_name) => vec!["--no-pager", "diff", "--no-ext", "-U1000000", orig_ref, ref_name, "--", &name],
@@ -127,6 +136,7 @@ fn main() {
             }
         );
 
+        // dispay two mypy result
         print_diff(&replaced_orig_mypy_string, &edit_mypy_string).expect("failed to write term");
     }
 
